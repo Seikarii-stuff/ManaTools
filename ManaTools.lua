dbc7804988c176d63423e3520b20ffee4f19e984
Seikarii <karlosseika@gmail.com>
2026-08-30 05:07:57 +0200

    Add /mana slash command to open ManaTools settings

--- ManaTools.lua
+++ ManaTools.lua
@@ -2,3 +2,22 @@ local ADDON_NAME, ManaTools = ...
 
 ManaTools = ManaTools or {}
 ManaTools.NoWasteCoin = ManaTools.NoWasteCoin or {}
+
+local function OpenManaToolsSettings()
+    if Settings and Settings.OpenToCategory and ManaTools.settingsCategory then
+        Settings.OpenToCategory(ManaTools.settingsCategory:GetID())
+        return
+    end
+
+    if InterfaceOptionsFrame_OpenToCategory then
+        InterfaceOptionsFrame_OpenToCategory("ManaTools")
+        InterfaceOptionsFrame_OpenToCategory("ManaTools")
+    end
+end
+
+SLASH_MANATOOLS1 = "/mana"
+SlashCmdList.MANATOOLS = function()
+    OpenManaToolsSettings()
+end
+
+ManaTools.OpenSettings = OpenManaToolsSettings
