4859d9893aeb913c6b03d8780ac7d7b2f85cd002
Seikarii <karlosseika@gmail.com>
2026-08-30 05:12:12 +0200

    Rename ManaTools.lua bootstrap to Bootstrap.lua

---
+++ Bootstrap.lua
@@ -0,0 +1,8 @@
+-- ManaTools bootstrap / shared addon namespace.
+-- Keep this file intentionally small: it runs before the feature modules.
+local ADDON_NAME, ManaTools = ...
+
+ManaTools = ManaTools or {}
+ManaTools.NoWasteCoin = ManaTools.NoWasteCoin or {}
+ManaTools.ADDON_NAME = ADDON_NAME
+ManaTools.VERSION = "1.1.0"
