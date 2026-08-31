0a12119a8699895aa924a4855fdc77a094aeb65c
Seikarii <karlosseika@gmail.com>
2026-08-31 04:40:07 +0200

    Initialize CinematicSkip in central DB

--- Bootstrap.lua
+++ Bootstrap.lua
@@ -7,7 +7,12 @@ ManaToolsDB = ManaToolsDB or {}
 ManaTools.DB = ManaToolsDB
 ManaTools.DB.NoWasteCoin = ManaTools.DB.NoWasteCoin or {}
 ManaTools.DB.ManaInvite = ManaTools.DB.ManaInvite or {}
+ManaTools.DB.CinematicSkip = ManaTools.DB.CinematicSkip or {}
+if ManaTools.DB.CinematicSkip.enabled == nil then
+    ManaTools.DB.CinematicSkip.enabled = true
+end
 ManaTools.NoWasteCoin = ManaTools.NoWasteCoin or {}
 ManaTools.ManaInvite = ManaTools.ManaInvite or {}
+ManaTools.CinematicSkip = ManaTools.CinematicSkip or {}
 ManaTools.ADDON_NAME = ADDON_NAME
 ManaTools.VERSION = "1.1.0"
