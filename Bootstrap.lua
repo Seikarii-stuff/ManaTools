ffd1a621c4de4431b15a881589239e745b1414b7
Seikarii <karlosseika@gmail.com>
2026-09-01 09:10:26 +0200

    NoInfo: add persistent enabled setting

--- Bootstrap.lua
+++ Bootstrap.lua
@@ -8,11 +8,16 @@ ManaTools.DB = ManaToolsDB
 ManaTools.DB.NoWasteCoin = ManaTools.DB.NoWasteCoin or {}
 ManaTools.DB.ManaInvite = ManaTools.DB.ManaInvite or {}
 ManaTools.DB.CinematicSkip = ManaTools.DB.CinematicSkip or {}
+ManaTools.DB.NoInfo = ManaTools.DB.NoInfo or {}
 if ManaTools.DB.CinematicSkip.enabled == nil then
     ManaTools.DB.CinematicSkip.enabled = true
+end
+if ManaTools.DB.NoInfo.enabled == nil then
+    ManaTools.DB.NoInfo.enabled = true
 end
 ManaTools.NoWasteCoin = ManaTools.NoWasteCoin or {}
 ManaTools.ManaInvite = ManaTools.ManaInvite or {}
 ManaTools.CinematicSkip = ManaTools.CinematicSkip or {}
+ManaTools.NoInfo = ManaTools.NoInfo or {}
 ManaTools.ADDON_NAME = ADDON_NAME
 ManaTools.VERSION = "1.1.0"
